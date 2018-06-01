24f80e30158090886d6d703bb2a0086b8de224a5
cyruslk <cyrus7@hotmail.fr>
2018-05-31 20:01:09 -0400

    Removing old socket api

--- src/api.js
+++
@@ -1,9 +0,0 @@
-// import openSocket from 'socket.io-client';
-// const  socket = openSocket('http://localhost:8000');
-
-// function changeMainData(cb) {
-//   socket.on('sendCMSData', data => cb(null, data));
-//   socket.emit('changeMainData');
-// }
-
-// export { changeMainData };
