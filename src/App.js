cabf7500543bf7bda1c8077aef23f241b39a9f4a
cyrus <cyrus7@hotmail.fr>
2020-07-26 10:46:51 -0400

    Refactor: formatting App.js

--- src/App.js
+++ src/App.js
@@ -36,6 +36,10 @@ class App extends Component {
   componentDidMount() {
 
     const color = getRandomColor();
+    const colorString1 = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    const colorString = `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.1)`;
+    const backgroundColorString = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    const colorDbCollectionName = color.join('');
 
     // init the stitch thing
     this.client = Stitch.initializeDefaultAppClient(stitchID);
@@ -50,12 +54,8 @@ class App extends Component {
     axios.get(spreadsheetURL)
     .then((response) => {
 
-    // format the data;
-    let dataWithColors = response.data.feed.entry.map(function(ele){
-    const colorString1 = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
-    const colorString = `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.1)`;
-    const backgroundColorString = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
-    const colorDbCollectionName = color.join('');
+    let dataWithColors = response.data.feed.entry.map((ele) => {
+
     const item = {
       name: ele.gsx$name.$t,
       info_secondaire: ele.gsx$infosecondaire.$t,
@@ -70,13 +70,36 @@ class App extends Component {
       colorString,
       colorDbCollectionName
     };
+     })
 
+     // adding everything in the state now;
       this.setState({
         data: dataWithColors,
-      });
-     })
+        colorString: colorString,
+        backgroundColorString: backgroundColorString
+      }, () => {
+        // console.log(this.state);
+      })
        }).catch((err) => {
        console.log(err);
+     });
+
+     axios.get(spreadsheetURL)
+     .then((response) => {
+       let dataHeader = response.data.feed.entry
+       .map((ele) => {
+         return {
+           name: ele.gsx$headerinfo.$t,
+           link: ele.gsx$headerlink.$t
+         };
+       }).filter(ele => ele.name.length > 0);
+       this.setState({
+         headerData: dataHeader
+       }, () => {
+         // console.log(this.state);
+       })
+     }).catch(error => {
+       console.log(error);
      });
    };
 
@@ -100,38 +123,184 @@ class App extends Component {
 
   render() {
 
+    const coloredBackgroundBlocsH1 = { color: 'white'};
+    const coloredBackgroundBlocsSpans = { color: 'white'};
+    const whiteBackgroundBlocsH1 = { color: this.state.backgroundColorString };
+    const whiteBackgroundBlocsSpans = { color: 'white' };
+    const { data, headerData, } = this.state;
 
-    const coloredBackgroundBlocsH1 = {
-      color: 'white',
-    };
-    const coloredBackgroundBlocsSpans = {
-      color: 'white',
-    };
-    const whiteBackgroundBlocsH1 = {
-      color: this.state.backgroundColorString,
-    };
-    const whiteBackgroundBlocsSpans = {
-      color: 'white',
+    if(!this.state.data){ return null }
+
+    const items = data.map((item, i) => {
+
+      console.log(item,i);
+
+      const stringToArrayName = item.item.name.split('');
+      const stringToArrayBluryText = item.item.name_end.split(' ');
+      const newArrayName = [];
+      const newArrayBluryText = [];
+      const classNameList = ['shake-slow', 'regular-span'];
+      const classNameListFilter = ['shake-slow', 'regular-span'];
+
+      function convertToSpans(oldArray, newArray, animArray) {
+      function returnRandomAnims(selectedAnimArray) {
+        return selectedAnimArray[Math.floor(Math.random() * selectedAnimArray.length)];
+      }
+      oldArray.map((oldArrayItem, index) => {
+        return newArray.push(
+          <span key={index} className={returnRandomAnims(animArray)}>
+            {oldArrayItem}
+          </span>
+        );
+      });
     };
 
-    const classNameList = ['shake-slow', 'regular-span'];
-    const classNameListFilter = ['shake-slow', 'regular-span'];
+
+    convertToSpans(stringToArrayName, newArrayName, classNameList);
+    convertToSpans(stringToArrayBluryText, newArrayBluryText, classNameListFilter);
 
 
-    if(!this.state.dbContent){
+    if (i % 2 === 0) {
       return (
-        <div>
-          loading
-        </div>
-      )
+
+        <section
+          key={i}
+          className="main_sections"
+          style={{
+            backgroundColor: item.backgroundColorString,
+          }}
+        >
+          <h1 key={i + 1 * 1} style={coloredBackgroundBlocsH1}>
+            {newArrayName}
+            <span style={coloredBackgroundBlocsSpans}>
+              {item.blury_text}
+            </span>
+            {newArrayBluryText}
+          </h1>
+          <GeneCanvas
+            key={i + 4 * 4}
+            zIndex={100}
+            position={"absolute"}
+            shouldDraw={false}
+            colorString= {this.state.colorString}
+            colorDbCollectionName="000"
+          />
+          <a className="block_links shake" style={coloredBackgroundBlocsSpans}
+            href={item.link} rel="noopener noreferrer" target="_blank">{item.info_secondaire}</a>
+        </section>
+      );
     }
-    console.log(this.state);
     return (
-      <div>
-      ---
+      <section key={i} className="main_sections">
+        <h1 key={i + 1 * 1} style={whiteBackgroundBlocsH1}>
+          {newArrayName}
+          <span style={whiteBackgroundBlocsSpans}>
+            {item.blury_text}
+          </span>
+          {newArrayBluryText}
+        </h1>
+        <GeneCanvas
+          key={i + 4 * 4}
+          zIndex={10000}
+          position={"absolute"}
+          shouldDraw={false}
+          background={false}
+          colorString= {this.state.colorString}
+          colorDbCollectionName="000"
+        />
+        <a className="block_links shake" style={whiteBackgroundBlocsH1}
+        href={item.link} rel="noopener noreferrer" target="_blank">{item.info_secondaire}</a>
+      </section>
+    );
+  });
+
+  const resultsRender = [];
+  for (let i = 0; i < items.length; i += 1) {
+    resultsRender.push(items[i]);
+    }
+
+
+  const coloredBackgroundHeader = {
+    color: 'white',
+    backgroundColor: this.state.backgroundColorString,
+    border: 'none',
+  };
+
+  const whiteBackgroundHeader = {
+    color: 'black',
+    backgroundColor: 'white',
+  };
+
+
+  const itemHeader = headerData.map((item, i) => {
+    if (i % 2) {
+      return (
+        <span key={i + 1 * 2}
+          style={coloredBackgroundHeader}
+        >
+        <a href={item.link} target="_blank">
+          {item.name}
+        </a>
+        </span>
+      );
+    }
+    return (
+      <span key={i + 2 * 4}
+        style={whiteBackgroundHeader}
+      >
+      <a href={item.link} target="_blank">
+        {item.name}
+      </a>
+      </span>
+    );
+  });
+
+  const resultsRenderHeader = [];
+  for (let i = 0; i < itemHeader.length; i += 1) {
+    resultsRenderHeader.push(itemHeader[i]);
+  }
+
+
+  // loading stuff;
+  if (data.length < 1) {
+    return (
+      <div className="loader_screen_container">
+      <div className="loader_span_container shake-little">
+            <span>LOADING</span>
+          </div>
       </div>
     );
   }
+  
+  return (
+    <div>
+    <div className="loader_screen_container canvas_loader">
+    <div className="loader_span_container shake-little">
+          <span>LOADING</span>
+        </div>
+    </div>
+    <div className="main_container">
+      <header>
+        {resultsRenderHeader}
+      </header>
+      <div className="resultsRender_container">
+      <GeneCanvas
+        key={4 * 4}
+        zIndex={1}
+        shouldDraw={false}
+        position={"fixed"}
+        colorString="rgba(0, 0, 0, 0.5)"
+        colorDbCollectionName="000"
+      />
+        {resultsRender}
+      </div>
+      <section className="credits">
+        code & design ---> &nbsp; <a href="http://www.c-t-l-k.com" target="_blank" rel="noopener noreferrer">www.c-t-l-k.com</a> &nbsp; (+ thanks to Conan Lai )
+      </section>
+    </div>
+    </div>
+  );
+}
 }
 
 export default App;
--- src/GeneCanvas.js
+++ src/GeneCanvas.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import is from "is_js";
+import is from 'is_js';
 
 class App extends Component {
+  throttledSetCanvasSize = this.throttle(this.setCanvasSize, 200);
+
   constructor(props) {
     super(props);
     this.canvas = React.createRef();
@@ -13,19 +15,135 @@ class App extends Component {
       shouldDraw: this.props.shouldDraw,
       shouldDisplayBackground: this.props.background,
       chromeOrFirefox: true,
-      lineWidth: 400
+      lineWidth: 400,
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
+    this.setCanvasSize();
+    window.addEventListener('resize', this.throttledSetCanvasSize);
+    this.dbColorString = this.props.colorString.slice(5, -1).split(', ').slice(0, 3).join('');
+    if (this.props.shouldDraw === true) {
+      this.fetchImages();
+    }
 
-    let ele = this.props.ele;
-    let name = ele.gsx$name.$t;
-    let bluryText = ele.gsx$blurytext.$t;
+    const isSafari = is.safari();
+    if (isSafari === true) {
+      this.setState({
+        lineWidth: 0,
+        chromeOrFirefox: false
+      });
+    }
+  }
 
+  setCanvasSize = () => {
+    const container = this.container.current;
+    const canvas = this.canvas.current;
+    const data = canvas.toDataURL();
+    const { width, height, } = container.getBoundingClientRect();
+    this.setState({
+      height,
+      width,
+    }, () => {
+      const context = canvas.getContext('2d');
+      const img = new Image();
+      img.src = data;
+      img.onload = () => {
+        context.drawImage(img, 0, 0, width, height);
+      };
+    });
+  }
+
+  fetchImages() {
+    const url = `/random/color/${this.dbColorString}`;
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      }).then((results) => {
+        if (results.length > 0) {
+          console.log('found image')
+          this.setState({
+            pickenImages: results[0],
+          }, () => {
+            this.populateImage();
+          });
+        }
+      });
+  }
+
+  populateImage = () => {
+    const img = new Image();
+    img.src = this.state.pickenImages;
+    img.onload = () => {
+      const canvas = this.canvas.current;
+      canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+  }
+
+  drawLine(x0, y0, x1, y1, color) {
+    if (this.state.chromeOrFirefox === true) {
+      const context = this.canvas.current.getContext('2d');
+      context.beginPath();
+      context.moveTo(x0, y0);
+      context.lineTo(x1, y1);
+      context.strokeStyle = color;
+      context.lineWidth = this.state.lineWidth;
+      context.lineCap = 'round';
+      context.lineJoin = 'round';
+      context.filter = 'blur(20px)';
+      context.stroke();
+      context.closePath();
+    } else {
+      return;
+    }
+  }
+
+  _onMouseOut = (e) => {
+    this.createImage();
+  }
+
+  _onMouseMove = (e) => {
+    const topOfElement = this.canvas.current.getBoundingClientRect().top + document.documentElement.scrollTop;
+    const x = e.pageX;
+    const y = e.pageY - topOfElement;
+    const currentCoords = {
+      x,
+      y,
+    };
+    const current = this.current || currentCoords;
+    this.drawLine(current.x, current.y, x, y, this.props.colorString, true);
+    this.current = currentCoords;
   }
 
 
+  _onMouseOver = (e) => {
+    if (!this.state.hasMousedOver
+      && this.state.shouldDraw) {
+      const canvas = this.canvas.current;
+      const context = canvas.getContext('2d');
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      this.setState({
+        hasMousedOver: true,
+      });
+    }
+  }
+
+  createImage() {
+    // REMOVED FOR NOW
+    const data = {
+      data: this.canvas.current.toDataURL(),
+    };
+
+    fetch(`/sendPic/${this.dbColorString}`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'content-type': 'application/json'
+      },
+    }).then((res) => {
+      console.log("successfully sent to db");
+    });
+  }
 
   throttle(callback, delay) {
     let previousCall = new Date().getTime();
@@ -42,20 +160,41 @@ class App extends Component {
   throttledDraw = this.throttle(this._onMouseMove, 100);
 
   render() {
+    const style = {
+      zIndex: this.props.zIndex,
+      height: '100%',
+      width: '100%',
+      position: this.props.position
+    };
 
-    let ele = this.props.ele;
-    let name = ele.gsx$name.$t;
-    let bluryText = ele.gsx$blurytext.$t;
-
-    if(!ele){
-      return null;
-    }
-
-      return (
-        <div className="main_container" ref={this.container}>
-          {name}
-        </div>
-      );
+    return (
+      <div
+        className="main_container"
+        ref={this.container}
+      >{
+          this.props.shouldDraw === true ? (
+            <canvas
+              width={this.state.width}
+              height={this.state.height}
+              className="canvas"
+              onMouseMove={this.throttledDraw}
+              onMouseOut={this._onMouseOut}
+              onMouseOver={this._onMouseOver}
+              style={style}
+              ref={this.canvas}
+            />
+          ) : (
+              <canvas
+                width={this.state.width}
+                height={this.state.height}
+                className="canvas"
+                style={style}
+                ref={this.canvas}
+              />
+            )
+        }
+      </div>
+    );
   }
 }
 
